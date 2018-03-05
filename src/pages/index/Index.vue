4b2c6733dadfa98fad89fcd9a5bd69f5b0040629
VampireKnight <33179411@qq.com>
2018-03-05 16:56:26 +0800

    20180305

--- src/pages/index/Index.vue
+++ src/pages/index/Index.vue
@@ -8,11 +8,15 @@
     </div>
     <footer class="mb-footer">
       <ul class="nav-list">
-        <router-link :to="{path: '/'}" tag="li"><i class="nvico"></i>首页</router-link>
-        <router-link :to="{path: '/courlis'}"  tag="li"><i class="nvico"></i>课程</router-link>
-        <router-link :to="{path: '/acadlis'}"  tag="li"><i class="nvico"></i>学院</router-link>
-        <router-link :to="{path: '/dyslis'}"   tag="li"><i class="nvico"></i>动态</router-link>
-        <router-link :to="{path: '/personal'}" tag="li"><i class="nvico"></i>个人中心</router-link>
+        <router-link :to="{path: '/'}" tag="li"><div class="iconWrap"><i class="nvico"></i></div><p>首页</p></router-link>
+        <router-link :to="{path: '/courlis'}"  tag="li"><div class="iconWrap"><i class="nvico"></i></div><p>课程</p></router-link>
+        <router-link :to="{path: '/acadlis'}"  tag="li"><div class="iconWrap"><i class="nvico"></i></div><p>学院</p></router-link>
+        <router-link :to="{path: '/dyslis'}"   tag="li"><div class="iconWrap"><i class="nvico"></i></div><p>动态</p></router-link>
+        <router-link :to="{path: '/personal'}" tag="li"><div class="iconWrap"><i class="nvico"></i></div><p>个人中心</p></router-link>
+                <!-- <div class="relpos">
+                  <i class="bnavli-myico bnav4"></i>
+                </div>
+                <p>会员</p> -->
       </ul>
     </footer>
   </div>
@@ -55,9 +59,28 @@ body{
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.nav-list .iconWrap{
+  height: 36px;
+}
+.nvico:after{
+  content:"";
+  height:36px;
+  width:36px;
+  margin-left:-18px;
+  position:absolute;
+} 
+.nvico:after {
+  background: url("../../../static/Img/Icon/bnav2.png") no-repeat;
+  background-size: 36px 36px; 
+}
+.router-link-exact-active .nvico:after{
+  background: url("../../../static/Img/Icon/bnav2-ac.png") no-repeat;
+  background-size: 36px 36px; 
 }
 </style>
--- src/router/index(abond).js
+++ src/router/index(abond).js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/pages/login/login'
-import Index from '@/pages/index/index'
-
-import Defind from '@/components/Index'
+/*import Index from '@/components/Index'*/
 import CouList from '@/components/CourLis'
 import AcaList from '@/components/AcadLis'
 import DysList from '@/components/DysLis'
 import Personal from '@/components/Personal'
-
+import Login from '@/pages/login/login'
+import Index from '@/pages/index/index'
 
 Vue.use(Router)
 
@@ -21,24 +19,22 @@ export default new Router({
 	     	component:Login
 	    },
 	    {
-	      	path: '/',
+	      	path: '/index',
 	      	name: '首页',
 	     	component:Index,
 	     	children:[
-	     		{path:"/index/index",name:"首页",component:Defind,alias:'/index'},
-	     		{path:"/index/courlis",name:"课程列表",component:CouList,alias:'/courlis'},
-	     		{path:"/index/acadlis",name:"学院列表",component:AcaList,alias:'/acadlis'},
-	     		{path:"/index/dyslis",name:"动态列表",component:DysList,alias:'/dyslis'},
-	     		{path:"/index/personal",name:"个人中心",component:Personal,alias:'/personal'}
-	     	],
-	     	redirect:"/index",
+	     		{path:"/index/courlis",name:"课程列表",component:CouList},
+	     		{path:"/index/acadlis",name:"学院列表",component:AcaList},
+	     		{path:"/index/dyslis",name:"动态列表",component:DysList},
+	     		{path:"/index/personal",name:"个人中心",component:Personal}
+	     	]
 	    },
 	    {
 	      	path: '/',
 	      	name: '首页',
 	     	component:Index,
 	    },
-/*	    {
+	    {
 	      	path: '/courlis',
 	      	name: '课程列表',
 	     	component:Index,
@@ -61,6 +57,6 @@ export default new Router({
 	      	name: '个人中心',
 	     	component:Index,
 	     	redirect:"/index/personal",
-	    }*/
+	    }
 	]
 })
